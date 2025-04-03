3ba068b50d70898642a093f2d5029d7b2ec66e4f
Arkadiusz Sas <kontakt.sas.arkadiusz@gmail.com>
2025-04-03 22:40:02 +0200

    Define the Producer creation schema

---
+++ kafka-basics-playground/kafka-basics/src/main/java/pl/encodeme/ProducerDemo.java
@@ -0,0 +1,19 @@
+package pl.encodeme;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ProducerDemo {
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
+    public static void main(String[] args) {
+        // 1. Create Producer Properties
+
+        // 2. Create the Producer
+
+        // 3. Send the data
+
+        // 4. Flush and close the Producer
+
+    }
+}
